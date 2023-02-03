eeec979d3b13ccb2a97b53c1de09bee1b3c8ddbb
Vilmantas Jakniūnas <42962270+Vilmokas@users.noreply.github.com>
2023-02-03 21:36:14 +0200

    feat: add root motvement

---
+++ Assets/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float rootSpeed = 5f;
+    public List<Root> roots;
+    public int currentRoot = 0;
+    public bool moveRoot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveRoot();
+    }
+
+    void MoveRoot()
+    {
+        if (moveRoot)
+        {
+            float right = Input.GetAxisRaw("Horizontal");
+            float up = Input.GetAxisRaw("Vertical");
+
+            if (right != 0 || up != 0)
+            {
+                roots[currentRoot].SetDirection(right, up);
+            }
+        }
+        else
+        {
+            roots[currentRoot].SetDirection(0, 0);
+        }
+    }
+}
---
+++ Assets/Root.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    public float speed = 5f;
+    public float up = 0;
+    public float right = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (up < 0)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        if (up > 0)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (right < 0)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (right > 0)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+
+    public void SetDirection(float r, float u)
+    {
+        if (right + r != 0)
+        {
+            right = r;
+        }
+        if (up + u != 0)
+        {
+            up = u;
+        }
+    }
+}
